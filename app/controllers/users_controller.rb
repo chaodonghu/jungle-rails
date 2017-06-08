70b2414961d740c256273453d32cebd23b85bbe1
Dong Hu <cdhu@uwaterloo.ca>
2017-06-08 20:13:17 +0000

    Add user saving logic to create action under users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,17 @@ class UsersController < ApplicationController
 
   # post new users
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user_id
+      redirect to '/'
+    else
+      redirect_to '/signup'
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
