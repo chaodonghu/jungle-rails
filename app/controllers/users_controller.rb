27967762b6de20539aa07bb3ed682127a731a881
Dong Hu <cdhu@uwaterloo.ca>
2017-06-08 19:04:20 +0000

    Create signup view file with form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  # get new users signup
+  def new
+  end
+
+  # post new users
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,13 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     # Admin/categories routes created (allows index, create(post) and new(get))
-    resources :categories, only: [:new, :index]
+    resources :categories, only: [:new, :create, :index]
   end
+
+  # Routes used for user signup. The first renders a form in the broswer, the second will receive the form
+  # and create a user in our database utilizing the data provided by the user
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
