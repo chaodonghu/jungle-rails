d5fef5e0c1b3f39b91f363ed9ba38dffe83b7d78
Dong Hu <cdhu@uwaterloo.ca>
2017-06-09 18:21:56 +0000

    Add validations to Review model & create pre-existing reviews to be displayed on products page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   has_many :orders
   has_many :reviews
 
+  validates :first_name,      presence: true
+  validates :last_name,       presence: true
+  validates :email,           presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
 end
