6cb4db0c66f9673899464f405b1100a6b440f526
Dong Hu <cdhu@uwaterloo.ca>
2017-06-09 18:37:20 +0000

    Create route to handle create (POST) request for views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :users
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create]
+
+  end
+  
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
